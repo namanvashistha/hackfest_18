c57b2a49237bd6fe16098da698189437ee0a5e06
namanvashistha <namanvashistha15@gmail.com>
2018-11-18 13:49:21 +0530

    added files

---
+++ js/home.js
@@ -0,0 +1,33 @@
+
+	function show_chat_box(){
+	var chat_box=document.getElementById('chat-box');
+	if(chat_box.style.display=="none")
+		chat_box.style.display='block';
+	else
+		chat_box.style.display='none';
+	}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+	
+
+var modal = document.getElementById('chat-box');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+	
