7188c2b8161710eb7ace356e8b372b1aa03b0d48
bhavyamendiratta <42599132+bhavyamendiratta@users.noreply.github.com>
2018-11-18 15:15:12 +0530

    index.php

---
+++ food_donation.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Food Donation</title>
+</head>
+<body>
+<p>This page is for food donation.</p>
+<p>Restaurants click here for entry of items</p>
+</body>
+</html>
